54d3f4a7f613cd74cf6cc906c3de8d89c3e5f501
Mike Chirico (Chico) <mchirico@t.aipiggybot.io>
2019-01-13 15:42:00 -0500

    Thread, clojure.data and work with nil

--- test/clojureslop/slop_test.clj
+++ test/clojureslop/slop_test.clj
@@ -1,7 +1,10 @@
 (ns clojureslop.slop-test
   (:require [midje.sweet :refer :all]
             [clojureslop.slop :refer :all])
+  ;(use 'clojure.data)
+  (:require [clojure.data :refer :all])
   )
+
 
 (println "Slop-test...")
 
@@ -11,3 +14,25 @@
              (:a z) => 3)
        )
 
+(facts "thread Test"
+       (fact "test"
+             (->> [1 2 3] (filter odd?) count) => 2)
+       ; Above and below are the same...
+       (->> [1 2 3] (filter odd?) count) => (count (filter odd? [1 2 3]))
+       )
+
+(facts "difference"
+  (fact "test"
+        (diff #{1 2} #{2 3}) => [#{1} #{3} #{2}]
+        (diff [1 2] [3 2 5]) => [[1] [3 nil 5] [nil 2]]
+        ; (true true) is a function, so need quote.
+        (filter identity (map nil? [1 nil 3 nil])) => '(true true)
+        ; Count nils
+        (count  (filter identity (map nil? [1 nil 3 nil]))) => 2
+        ; Index of nil
+        (.indexOf (map nil? [0 nil 3 2] ) true) => 1
+
+        ; Careful .. Don't forget the arrow... will always be true
+        (.indexOf (map nil? [0 nil 3 2] ) true) = 7
+        )
+       )
